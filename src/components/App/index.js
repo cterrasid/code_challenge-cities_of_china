5fc05ef9fdc64e6c453e2b3d88e3cdb05fea1062
cterrasid <clarette.terrasi@gmail.com>
2019-10-01 21:06:18 +0200

    Improve data painting and html structure

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, Fragment } from 'react';
 import data from '../../data/cities-of-china.json';
 import './styles.scss';
 
@@ -27,55 +27,62 @@ class App extends PureComponent {
 
 	render() {
 		console.log(this.state);
-		const {data} = this.state;
+		const { data } = this.state;
 
 		return (
-			<section className="app__container">
-				{/*HEADER*/}
-				<header className="header__container">Cities of China</header>
-				{/**FILTERS */}
-				<form className="filter__container">
-					<label className="filter-name__label" htmlFor="filter-name" title="Search by name">
-						<input
-							className="filter__name-input"
-							id="filter-name"
-							type="text"
-							placeholder="Search by name"
-						/>
-					</label>
-					<label className="filter__select" htmlFor="select-all" title="Select all">
-						<input type="checkbox" id="select-all" value="filter__select-all" />
-						Select All
-					</label>
-				</form>
-				{/**DATA */}
-				<section className="data__container">
-					<ul className="data__element">
-					  {data.map(city => {
-					    return (
-					   <label htmlFor="select-city">
-					    <input
-					      id="select-city"
-					      type="checkbox"
-					      value="select-city"
-					      name="select-city"
-					    />
-					  </label>
-					    <li key={city.id}>
-					      <Card id={city.id} name={city.name}/>
-					    </li>
-					  )})}
-					</ul>
-			</section>
-				{/*SELECTION */}
-				<section className="results__container">
-					<p className="results__counter">x items selected</p>
-					<button className="results__clear" type="button">
-						Clear
-					</button>
+			<Fragment>
+				<section className="app__container">
+					{/*HEADER*/}
+					<header className="header__container">Cities of China</header>
+					{/**FILTERS */}
+					<form className="filter__container">
+						<label className="filter-name__label" htmlFor="filter-name" title="Search by name">
+							<input
+								className="filter__name-input"
+								id="filter-name"
+								type="text"
+								placeholder="Search by name"
+							/>
+						</label>
+						<label className="filter__select" htmlFor="select-all" title="Select all">
+							<input type="checkbox" id="select-all" value="filter__select-all" />
+							Select All
+						</label>
+					</form>
+					{/**DATA */}
+					<section className="data__container">
+						<ul className="data__city-list">
+							{data.map(city => {
+								return (
+									<li key={city.id}>
+										<label htmlFor="select-city">
+											<input
+												id="select-city"
+												type="checkbox"
+												value="select-city"
+												name={city.name}
+											/>
+										</label>
+										<img className="city__logo" src="/" alt="Imagen de muestra" />
+                    <div className="city__data-wrapper">
+                      <p className="city__name">{city.name}</p>
+                      <p className="city__chinese-name">{city.chineseName}</p>
+                    </div>
+									</li>
+								);
+							})}
+						</ul>
+					</section>
+					{/*SELECTION */}
+					<section className="results__container">
+						<p className="results__counter">x items selected</p>
+						<button className="results__clear" type="button">
+							Clear
+						</button>
+					</section>
+					<section className="selection__container">Selection</section>
 				</section>
-				<section className="selection__container">Selection</section>
-			</section>
+			</Fragment>
 		);
 	}
 }
