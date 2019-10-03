50ba9569b38d48f042ef62bcbe16f2decfcc0333
clarettetd <48140509+cterrasid@users.noreply.github.com>
2019-10-03 15:28:00 +0200

    Create index.js

---
+++ src/components/ResultData/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Counter from '../Counter';
+import Selection from '../Selection';
+import './styles.scss';
+
+const ResultData = () => {
+
+  return (
+    <section className="result-data__container">
+      <Counter />
+      <Selection />
+    </section>
+  );
+};
+
+export default ResultData;
