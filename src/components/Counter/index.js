5a971e8416a6da31eb447f269a92b0886484ac54
clarettetd <48140509+cterrasid@users.noreply.github.com>
2019-10-03 15:36:20 +0200

    Create index.js

---
+++ src/components/Counter/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.scss';
+
+const Counter = () => {
+  return (
+    <section className="counter__container">
+      <p className="counter__counter">x items selected</p>
+      <button className="counter__clear-button" type="button">
+        Clear
+      </button>
+    </section>
+  );
+};
+
+export default Counter;
